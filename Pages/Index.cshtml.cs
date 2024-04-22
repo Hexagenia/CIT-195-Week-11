0a0c5ef416a5b46cab130c66e1de47507d345698
Colin Elliott <79231253+Hexagenia@users.noreply.github.com>
2024-04-22 18:46:38 -0400

    Add files via upload

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPagesMovie.Pages
+namespace ContosoUniversity.Pages
 {
     public class IndexModel : PageModel
     {
